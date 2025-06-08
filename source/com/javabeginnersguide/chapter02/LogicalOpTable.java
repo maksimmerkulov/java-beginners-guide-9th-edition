0845a1de324d6bb5e305fd91c1a241b850e86a51
maksimmerkulov <merkulov.mv@outlook.com>
2025-06-08 17:30:00 +0500

    Глава 2: Старшинство операций (Введение в типы данных и операции)

--- source/com/javabeginnersguide/chapter02/LogicalOpTable.java
+++ source/com/javabeginnersguide/chapter02/LogicalOpTable.java
@@ -1,31 +1,34 @@
 package com.javabeginnersguide.chapter02;
 
-// Упражнение 2.2. Вывод таблицы истинности для логических операций.
+/*
+  Упражнение 2.2. Дополнительная задача.
+  Вывод таблицы истинности для логических операций.
+*/
 class LogicalOpTable {
     public static void main(String[] args) {
 
-        boolean p, q;
+        int p, q;
 
         System.out.println("P\tQ\tAND\tOR\tXOR\tNOT");
 
-        p = true; q = true;
+        p = 1; q = 1;
         System.out.print(p + "\t" + q +"\t");
         System.out.print((p&q) + "\t" + (p|q) + "\t");
-        System.out.println((p^q) + "\t" + (!p));
+        System.out.println((p^q) + "\t" + (1 - p));
 
-        p = true; q = false;
+        p = 1; q = 0;
         System.out.print(p + "\t" + q +"\t");
         System.out.print((p&q) + "\t" + (p|q) + "\t");
-        System.out.println((p^q) + "\t" + (!p));
+        System.out.println((p^q) + "\t" + (1 - p));
 
-        p = false; q = true;
+        p = 0; q = 1;
         System.out.print(p + "\t" + q +"\t");
         System.out.print((p&q) + "\t" + (p|q) + "\t");
-        System.out.println((p^q) + "\t" + (!p));
+        System.out.println((p^q) + "\t" + (1 - p));
 
-        p = false; q = false;
+        p = 0; q = 0;
         System.out.print(p + "\t" + q +"\t");
         System.out.print((p&q) + "\t" + (p|q) + "\t");
-        System.out.println((p^q) + "\t" + (!p));
+        System.out.println((p^q) + "\t" + (1 - p));
     }
 }
