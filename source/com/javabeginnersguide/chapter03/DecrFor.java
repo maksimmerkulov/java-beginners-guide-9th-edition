2a18b6e56aacfa541574f080f99cec6543e89ddb
maksimmerkulov <merkulov.mv@outlook.com>
2025-06-15 15:30:00 +0500

    Глава 3: Цикл `for` (Операторы управления программой)

---
+++ source/com/javabeginnersguide/chapter03/DecrFor.java
@@ -0,0 +1,11 @@
+package com.javabeginnersguide.chapter03;
+
+// Цикл for, выполняющийся в отрицательном направлении.
+class DecrFor {
+    public static void main(String[] args) {
+        int x;
+
+        for(x = 100; x > -100; x -= 5)
+            System.out.println(x);
+    }
+}
