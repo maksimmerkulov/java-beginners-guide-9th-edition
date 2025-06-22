5a863cfcf1a23035bec0a7d206ed5f70b0b9e90b
maksimmerkulov <merkulov.mv@outlook.com>
2025-06-22 16:15:00 +0500

    Глава 4: Параметризованные конструкторы (Введение в классы, объекты и методы)

---
+++ source/com/javabeginnersguide/chapter04/ParmConsDemo.java
@@ -0,0 +1,20 @@
+package com.javabeginnersguide.chapter04;
+
+// Параметризованный конструктор.
+
+class MyClass {
+    int x;
+
+    MyClass(int i) {
+        x = i;
+    }
+}
+
+class ParmConsDemo {
+    public static void main(String[] args) {
+        MyClass t1 = new MyClass(10);
+        MyClass t2 = new MyClass(88);
+
+        System.out.println(t1.x + " " + t2.x);
+    }
+}
