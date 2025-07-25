402b3c1dd72e8f9b9d1210f2df69f90c34cb07b9
maksimmerkulov <merkulov.mv@outlook.com>
2025-07-25 17:35:38 +0500

    Глава 1: Вопросы и упражнения для самопроверки (Основы языка Java)

---
+++ src/chapter01/Moon.java
@@ -0,0 +1,17 @@
+package chapter01;
+
+/*
+  Расчет вашего фактического веса на Луне.
+  Назовите этот файл Moon.java.
+*/
+class Moon {
+    public static void main(String[] args) {
+        double earthweight; // вес на Земле
+        double moonweight;  // вес на Луне
+        earthweight = 165;
+        moonweight = earthweight * 0.17;
+        System.out.println("Ваш вес " + earthweight +
+                " в земных фунтах эквивалентен весу " +
+                moonweight + " в лунных фунтах.");
+    }
+}
