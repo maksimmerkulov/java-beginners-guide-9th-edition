0b44a4f87a7dd2064c00e96ee40d1b8ffde32ff0
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-19 19:54:53 +0500

    Глава 3: Пропуск частей (Операторы управления программой)

---
+++ src/chapter03/Empty.java
@@ -0,0 +1,12 @@
+package chapter03;
+
+// Части цикла for могут быть пустыми.
+class Empty {
+    public static void main(String[] args) {
+        int i;
+        for(i = 0; i < 10; ) {
+            System.out.println("Проход #" + i);
+            i++; // инкрементирование переменной управления циклом
+        }
+    }
+}
