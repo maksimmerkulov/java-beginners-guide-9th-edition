adba07e6cf1170d711507ca3937376c07a10cc1a
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-21 15:28:18 +0500

    Глава 3: Использование оператора `break` как разновидности перехода в стиле "goto" (Операторы управления программой)

---
+++ src/chapter03/Break6.java
@@ -0,0 +1,27 @@
+package chapter03;
+
+// Место размещения метки очень важно.
+class Break6 {
+    public static void main(String[] args) {
+        int x=0, y=0;
+
+        // Метка находится перед оператором for.
+        stop1: for(x=0; x < 5; x++) {
+            for(y = 0; y < 5; y++) {
+                if(y == 2) break stop1;
+                System.out.println("x и y: " + x + " " + y);
+            }
+        }
+
+        System.out.println();
+
+        // Метка находится непосредственно перед {.
+        for(x=0; x < 5; x++)
+            stop2: {
+                for(y = 0; y < 5; y++) {
+                    if(y == 2) break stop2;
+                    System.out.println("x и y: " + x + " " + y);
+                }
+            }
+    }
+}
