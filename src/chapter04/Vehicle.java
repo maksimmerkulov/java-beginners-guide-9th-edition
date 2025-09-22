3ee0d42ee41fb68b66af895ac7aac1478834e6b0
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-22 16:03:42 +0500

    Глава 4: Основы классов (Введение в классы, объекты и методы)

---
+++ src/chapter04/Vehicle.java
@@ -0,0 +1,9 @@
+package chapter04;
+
+// В этой программе создаются два объекта Vehicle.
+
+class Vehicle {
+    int passengers; // количество пассажиров
+    int fuelcap;    // запас топлива в галлонах
+    int mpg;        // расход топлива в милях на галлон
+}
