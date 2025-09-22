dd69959f62b46c81fc83d7c6b9cfb13e41add939
maksimmerkulov <merkulov.mv@outlook.com>
2025-09-22 16:49:26 +0500

    Глава 4: Методы (Введение в классы, объекты и методы)

--- src/chapter04/Vehicle.java
+++ src/chapter04/Vehicle.java
@@ -1,9 +1,14 @@
 package chapter04;
 
-// В этой программе создаются два объекта Vehicle.
+// Добавление в класс Vehicle метода range().
 
 class Vehicle {
     int passengers; // количество пассажиров
     int fuelcap;    // запас топлива в галлонах
     int mpg;        // расход топлива в милях на галлон
+
+    // Отображает дальность поездки.
+    void range() {
+        System.out.println("Дальность поездки в милях: " + fuelcap * mpg);
+    }
 }
