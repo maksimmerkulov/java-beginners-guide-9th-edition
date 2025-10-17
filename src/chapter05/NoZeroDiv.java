a2d2969b9ce031d5d3ebfa24583d67ac7a2cad9f
maksimmerkulov <merkulov.mv@outlook.com>
2025-10-17 17:03:48 +0500

    Глава 5: Операция `?` (Дополнительные сведения о типах данных и операциях)

---
+++ src/chapter05/NoZeroDiv.java
@@ -0,0 +1,13 @@
+package chapter05;
+
+// Предотвращает деление на 0 с использованием операции ?.
+class NoZeroDiv {
+    public static void main(String[] args) {
+        int result;
+        for(int i = -5; i < 6; i++) {
+            result = i != 0 ? 100 / i : 0;
+            if(i != 0)
+                System.out.println("100 / " + i + " равно " + result);
+        }
+    }
+}
---
+++ src/chapter05/NoZeroDiv2.java
@@ -0,0 +1,11 @@
+package chapter05;
+
+// Предотвращает деление на 0 с использованием операции ?.
+class NoZeroDiv2 {
+    public static void main(String[] args) {
+
+        for(int i = -5; i < 6; i++)
+            if(i != 0 ? true : false)
+                System.out.println("100 / " + i + " равно " + 100 / i);
+    }
+}
