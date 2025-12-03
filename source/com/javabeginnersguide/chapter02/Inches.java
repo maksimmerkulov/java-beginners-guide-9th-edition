22eed5d2b92fe60798f3c03da6c6fa376a590d93
maksimmerkulov <merkulov.mv@outlook.com>
2025-12-03 15:03:22 +0500

    Глава 2: Примитивные типы Java (Введение в типы данных и операции)

---
+++ source/com/javabeginnersguide/chapter02/Inches.java
@@ -0,0 +1,15 @@
+package com.javabeginnersguide.chapter02;
+
+/*
+  Вычисляет количество кубических дюймов в кубической миле.
+*/
+class Inches {
+    public static void main(String[] args) {
+        long ci;
+        long im;
+        im = 5280 * 12;
+        ci = im * im * im;
+        System.out.println("В кубической миле содержится " + ci +
+                " кубических дюймов.");
+    }
+}
