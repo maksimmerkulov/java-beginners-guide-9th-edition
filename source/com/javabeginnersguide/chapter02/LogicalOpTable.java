143600b1fbd301f5815e3d3c47e72cf6823ae1a8
maksimmerkulov <merkulov.mv@outlook.com>
2025-12-03 17:08:20 +0500

    Глава 2: Старшинство операций (Введение в типы данных и операции)

---
+++ source/com/javabeginnersguide/chapter02/LogicalOpTable.java
@@ -0,0 +1,31 @@
+package com.javabeginnersguide.chapter02;
+
+// Упражнение 2.2. Вывод таблицы истинности для логических операций.
+class LogicalOpTable {
+    public static void main(String[] args) {
+
+        int p, q;
+
+        System.out.println("P\tQ\tAND\tOR\tXOR\tNOT");
+
+        p = 1; q = 1;
+        System.out.print(p + "\t" + q +"\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (1 - p));
+
+        p = 1; q = 0;
+        System.out.print(p + "\t" + q +"\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (1 - p));
+
+        p = 0; q = 1;
+        System.out.print(p + "\t" + q +"\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (1 - p));
+
+        p = 0; q = 0;
+        System.out.print(p + "\t" + q +"\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
+        System.out.println((p^q) + "\t" + (1 - p));
+    }
+}
