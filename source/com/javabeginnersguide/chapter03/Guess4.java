b758cf618a32d5678bbf0cf7537fe800198ab3c9
maksimmerkulov <merkulov.mv@outlook.com>
2025-12-05 21:25:12 +0500

    Глава 3: Цикл `do-while` (Операторы управления программой)

---
+++ source/com/javabeginnersguide/chapter03/Guess4.java
@@ -0,0 +1,28 @@
+package com.javabeginnersguide.chapter03;
+
+// Игра в угадывание буквы, версия 4.
+class Guess4 {
+    public static void main(String[] args)
+            throws java.io.IOException {
+        char ch, ignore, answer = 'K';
+        do {
+            System.out.println("Задумана буква между A и Z.");
+            System.out.print("Попробуйте ее угадать: ");
+
+            // Прочитать символ.
+            ch = (char) System.in.read();
+
+            // Отбросить все остальные символы из буфера ввода.
+            do {
+                ignore = (char) System.in.read();
+            } while(ignore != '\n');
+            if(ch == answer) System.out.println("** Правильно **");
+            else {
+                System.out.print("...Увы, не угадали. Задуманная буква находится ");
+                if(ch < answer) System.out.println("дальше по алфавиту.");
+                else System.out.println("ближе по алфавиту.");
+                System.out.println("Повторите попытку!\n");
+            }
+        } while(answer != ch);
+    }
+}
