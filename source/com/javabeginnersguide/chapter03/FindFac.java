ff236d18aec46b13a8b8d61b57c94204884c5062
maksimmerkulov <merkulov.mv@outlook.com>
2025-12-05 22:15:05 +0500

    Глава 3: Вложенные циклы (Операторы управления программой)

---
+++ source/com/javabeginnersguide/chapter03/FindFac.java
@@ -0,0 +1,16 @@
+package com.javabeginnersguide.chapter03;
+
+/*
+  Использование вложенного цикла для нахождения множителей чисел от 2 до 100.
+*/
+class FindFac {
+    public static void main(String[] args) {
+
+        for(int i=2; i <= 100; i++) {
+            System.out.print("Множители " + i + ": ");
+            for(int j = 2; j <= i/2; j++)
+                if((i%j) == 0) System.out.print(j + " ");
+            System.out.println();
+        }
+    }
+}
