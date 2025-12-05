d4515f1d89e4e494d80855287ebdbb02d956059a
maksimmerkulov <merkulov.mv@outlook.com>
2025-12-05 20:36:27 +0500

    Глава 3: Цикл `for` (Операторы управления программой)

---
+++ source/com/javabeginnersguide/chapter03/SqrRoot.java
@@ -0,0 +1,18 @@
+package com.javabeginnersguide.chapter03;
+
+// Отображение квадратных корней чисел от 1 до 99 и ошибки округления.
+class SqrRoot {
+    public static void main(String[] args) {
+        double num, sroot, rerr;
+
+        for(num = 1.0; num < 100.0; num++) {
+            sroot = Math.sqrt(num);
+            System.out.println("Квадратный корень числа " + num +
+                    " равен " + sroot);
+            // Вычислить ошибку округления.
+            rerr = num - (sroot * sroot);
+            System.out.println("Ошибка округления составляет " + rerr);
+            System.out.println();
+        }
+    }
+}
