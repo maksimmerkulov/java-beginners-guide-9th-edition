30a2ae26ef7397edf3ce6168d42227769ff88210
maksimmerkulov <merkulov.mv@outlook.com>
2025-12-05 22:12:01 +0500

    Глава 3: Использование оператора `continue` (Операторы управления программой)

---
+++ source/com/javabeginnersguide/chapter03/ContToLabel.java
@@ -0,0 +1,17 @@
+package com.javabeginnersguide.chapter03;
+
+// Использование оператора continue с меткой.
+class ContToLabel {
+    public static void main(String[] args) {
+
+        outerloop:
+        for(int i=1; i < 10; i++) {
+            System.out.print("\nПроход внешнего цикла #" + i +
+                    ", внутренний цикл: ");
+            for(int j = 1; j < 10; j++) {
+                if(j == 5) continue outerloop;  // продолжить внешний цикл
+                System.out.print(j);
+            }
+        }
+    }
+}
