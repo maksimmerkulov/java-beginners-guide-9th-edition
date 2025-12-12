f5d222bc518137d2a316455cf62aba2cdf7ecc38
maksimmerkulov <merkulov.mv@outlook.com>
2025-12-12 18:11:47 +0500

    Глава 2: Область видимости и время жизни переменных (Введение в типы данных и операции)

---
+++ source/com/javabeginnersguide/chapter02/VarInitDemo.java
@@ -0,0 +1,15 @@
+package com.javabeginnersguide.chapter02;
+
+// Демонстрация времени жизни переменной.
+class VarInitDemo {
+    public static void main(String[] args) {
+        int x;
+
+        for(x = 0; x < 3; x++) {
+            int y = -1; // y инициализируется при каждом входе в блок
+            System.out.println("Значение y равно: " + y); // всегда выводится -1
+            y = 100;
+            System.out.println("Теперь значение y равно: " + y);
+        }
+    }
+}
