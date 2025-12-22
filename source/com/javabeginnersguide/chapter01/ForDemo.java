d8783bfd1316faf300c4f954630d698035c1e877
maksimmerkulov <merkulov.mv@outlook.com>
2025-12-22 16:24:13 +0500

    Глава 1: Два управляющих оператора (Основы языка Java)

--- source/com/javabeginnersguide/chapter01/ForDemo.java
+++ source/com/javabeginnersguide/chapter01/ForDemo.java
@@ -8,7 +8,7 @@ class ForDemo {
     public static void main(String[] args) {
         int count;
 
-        for(count = 0; count < 5; count = count+1)
+        for(count = 0; count < 5; count++)
             System.out.println("Значение count: " + count);
 
         System.out.println("Готово!");
