2b89fc507916d0c3948757d94fdf6b0df549035a
maksimmerkulov <merkulov.mv@outlook.com>
2026-01-02 19:23:09 +0500

    Глава 3: Использование оператора `continue` (Операторы управления программой)

---
+++ source/com/javabeginnersguide/chapter03/ContDemo.java
@@ -0,0 +1,13 @@
+package com.javabeginnersguide.chapter03;
+
+// Использование оператора continue.
+class ContDemo {
+    public static void main(String[] args) {
+        int i;
+        // Вывести четные числа между 0 и 100.
+        for(i = 0; i<=100; i++) {
+            if((i%2) != 0) continue;    // следующая итерация
+            System.out.println(i);
+        }
+    }
+}
