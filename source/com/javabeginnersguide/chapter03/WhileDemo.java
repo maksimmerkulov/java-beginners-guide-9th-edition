f304d1fe5aaa24809bb525cb253a10f0b45075d9
maksimmerkulov <merkulov.mv@outlook.com>
2026-01-02 17:17:22 +0500

    Глава 3: Цикл `while` (Операторы управления программой)

---
+++ source/com/javabeginnersguide/chapter03/WhileDemo.java
@@ -0,0 +1,14 @@
+package com.javabeginnersguide.chapter03;
+
+// Демонстрация применения цикла while.
+class WhileDemo {
+    public static void main(String[] args) {
+        char ch;
+        // Вывести буквы английского алфавита, используя цикл while.
+        ch = 'a';
+        while(ch <= 'z') {
+            System.out.print(ch);
+            ch++;
+        }
+    }
+}
