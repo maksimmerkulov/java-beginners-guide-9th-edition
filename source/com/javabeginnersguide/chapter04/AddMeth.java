da1ae3cc63e1161113706494563fe52cd16aab71
maksimmerkulov <merkulov.mv@outlook.com>
2026-01-05 17:11:40 +0500

    Глава 4: Методы (Введение в классы, объекты и методы)

---
+++ source/com/javabeginnersguide/chapter04/AddMeth.java
@@ -0,0 +1,41 @@
+package com.javabeginnersguide.chapter04;
+
+// Добавление в класс Vehicle метода range().
+
+class Vehicle {
+    int passengers; // количество пассажиров
+    int fuelcap;    // запас топлива в галлонах
+    int mpg;        // расход топлива в милях на галлон
+
+    // Отображает дальность поездки.
+    void range() {
+        System.out.println("Дальность поездки в милях: " + fuelcap * mpg);
+    }
+}
+
+class AddMeth {
+    public static void main(String[] args) {
+        Vehicle minivan = new Vehicle();
+        Vehicle sportscar = new Vehicle();
+
+        int range1, range2;
+
+        // Присвоить значения полям в minivan.
+        minivan.passengers = 7;
+        minivan.fuelcap = 16;
+        minivan.mpg = 21;
+
+        // Присвоить значения полям в sportscar.
+        sportscar.passengers = 2;
+        sportscar.fuelcap = 14;
+        sportscar.mpg = 12;
+
+        System.out.print("Минивэн может перевезти " + minivan.passengers +
+                " пассажиров. ");
+        minivan.range();    // отобразить дальность поездки для minivan
+
+        System.out.print("Спортивный автомобиль может перевезти " +
+                sportscar.passengers + " пассажиров. ");
+        sportscar.range();  // отобразить дальность поездки для sportscar
+    }
+}
