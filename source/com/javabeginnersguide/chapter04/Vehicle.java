bf32cbe4e95ab789003181afbdd40a559afcfa7d
maksimmerkulov <merkulov.mv@outlook.com>
2026-01-05 16:19:10 +0500

    Глава 4: Основы классов (Введение в классы, объекты и методы)

---
+++ source/com/javabeginnersguide/chapter04/Vehicle.java
@@ -0,0 +1,7 @@
+package com.javabeginnersguide.chapter04;
+
+class Vehicle {
+    int passengers; // количество пассажиров
+    int fuelcap;    // запас топлива в галлонах
+    int mpg;        // расход топлива в милях на галлон
+}
