9a746f97428ddcd634a8dc2a7b904d46eb14f889
maksimmerkulov <merkulov.mv@outlook.com>
2026-01-21 16:57:15 +0500

    Глава 5: Цикл `for` в стиле "for-each" (Дополнительные сведения о типах данных и операциях)

---
+++ source/com/javabeginnersguide/chapter05/NoChange.java
@@ -0,0 +1,20 @@
+package com.javabeginnersguide.chapter05;
+
+// Цикл for в стиле "for-each" по существу допускает только чтение.
+class NoChange {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        for(int x : nums) {
+            System.out.print(x + " ");
+            x = x * 10;
+        }
+
+        System.out.println();
+
+        for(int x : nums)
+            System.out.print(x + " ");
+
+        System.out.println();
+    }
+}
