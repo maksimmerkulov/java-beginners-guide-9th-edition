9af0acebc90ef8b0fdcee06715d54a6b11baa341
maksimmerkulov <merkulov.mv@outlook.com>
2026-01-21 16:52:31 +0500

    Глава 5: Цикл `for` в стиле "for-each" (Дополнительные сведения о типах данных и операциях)

---
+++ source/com/javabeginnersguide/chapter05/ForEach.java
@@ -0,0 +1,18 @@
+package com.javabeginnersguide.chapter05;
+
+// Использование цикла for в стиле "for-each".
+class ForEach {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int sum = 0;
+
+        // Применение цикла for в стиле "for-each" для отображения
+        // и суммирования значений.
+        for(int x : nums) {
+            System.out.println("Значение: " + x);
+            sum += x;
+        }
+
+        System.out.println("Сумма: " + sum);
+    }
+}
