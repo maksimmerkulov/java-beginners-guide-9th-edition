0d418a9cd024e6912ddfbce991be70100e67ea97
maksimmerkulov <merkulov.mv@outlook.com>
2026-02-08 17:37:46 +0500

    Глава 1: Первая простая программа (Основы языка Java)

---
+++ source/com/javabeginnersguide/chapter01/Example.java
@@ -0,0 +1,12 @@
+package com.javabeginnersguide.chapter01;
+
+/*
+  Простая программа на Java.
+  Назовите этот файл Example.java.
+*/
+class Example {
+    // Программа на Java начинается с вызова main().
+    public static void main(String[] args) {
+        System.out.println("Java движет веб-сетью.");
+    }
+}
