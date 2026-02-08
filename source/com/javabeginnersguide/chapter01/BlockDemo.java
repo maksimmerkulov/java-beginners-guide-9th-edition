ed26f54fa51079d8786916e3cd037549cee1e36f
maksimmerkulov <merkulov.mv@outlook.com>
2026-02-08 18:27:30 +0500

    Глава 1: Создание блоков кода (Основы языка Java)

---
+++ source/com/javabeginnersguide/chapter01/BlockDemo.java
@@ -0,0 +1,20 @@
+package com.javabeginnersguide.chapter01;
+
+/*
+  Демонстрация использования блока кода.
+  Назовите этот файл BlockDemo.java.
+*/
+class BlockDemo {
+    public static void main(String[] args) {
+        double i, j, d;
+        i = 5;
+        j = 10;
+
+        // Целью оператора if является блок.
+        if(i != 0) {
+            System.out.println("Значение i не равно нулю.");
+            d = j / i;
+            System.out.println("Результат j / i равен " + d);
+        }
+    }
+}
