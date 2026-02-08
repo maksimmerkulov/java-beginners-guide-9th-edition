f39202f393e8b27e82adcde4797ee4d5428c8fac
maksimmerkulov <merkulov.mv@outlook.com>
2026-02-08 18:21:31 +0500

    Глава 1: Два управляющих оператора (Основы языка Java)

---
+++ source/com/javabeginnersguide/chapter01/ForDemo.java
@@ -0,0 +1,16 @@
+package com.javabeginnersguide.chapter01;
+
+/*
+  Демонстрация использования цикла for.
+  Назовите этот файл ForDemo.java.
+*/
+class ForDemo {
+    public static void main(String[] args) {
+        int count;
+
+        for(count = 0; count < 5; count = count+1)
+            System.out.println("Значение count: " + count);
+
+        System.out.println("Готово!");
+    }
+}
