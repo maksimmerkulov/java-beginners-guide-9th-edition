bd8256b7d1db34566866e4cfdd822270935c7df4
maksimmerkulov <merkulov.mv@outlook.com>
2026-02-15 18:09:12 +0500

    Глава 2: Примитивные типы Java (Введение в типы данных и операции)

---
+++ source/com/javabeginnersguide/chapter02/Hypot.java
@@ -0,0 +1,18 @@
+package com.javabeginnersguide.chapter02;
+
+/*
+  Использование теоремы Пифагора для нахождения
+  длины гипотенузы по длинам двух катетов.
+*/
+class Hypot {
+    public static void main(String[] args) {
+        double x, y, z;
+
+        x = 3;
+        y = 4;
+
+        z = Math.sqrt(x*x + y*y);
+
+        System.out.println("Длина гипотенузы: " +z);
+    }
+}
