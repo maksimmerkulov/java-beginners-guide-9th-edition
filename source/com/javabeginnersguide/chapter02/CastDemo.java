c253b2f125f39829aa13edc31b8669890a13bacf
maksimmerkulov <merkulov.mv@outlook.com>
2026-02-15 18:41:40 +0500

    Глава 2: Приведение несовместимых типов (Введение в типы данных и операции)

---
+++ source/com/javabeginnersguide/chapter02/CastDemo.java
@@ -0,0 +1,32 @@
+package com.javabeginnersguide.chapter02;
+
+// Демонстрация приведений.
+class CastDemo {
+    public static void main(String[] args) {
+        double x, y;
+        byte b;
+        int i;
+        char ch;
+
+        x = 10.0;
+        y = 3.0;
+
+        i = (int) (x / y);  // приведение double к int
+        System.out.println("Целочисленный результат x / y: " + i);
+
+        i = 100;
+        b = (byte) i;
+
+        System.out.println("Значение b: " + b);
+
+        i = 257;
+        b = (byte) i;
+
+        System.out.println("Значение b: " + b);
+
+        b = 88; // код ASCII для X
+        ch = (char) b;
+
+        System.out.println("Значение ch: " + ch);
+    }
+}
