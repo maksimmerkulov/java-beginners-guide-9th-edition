86cc58ac0c8fc612a3c4b223768ea38f33d8c62e
maksimmerkulov <merkulov.mv@outlook.com>
2026-03-15 10:30:00 +0500

    Глава 3: Оператор `if` (Операторы управления программой)

---
+++ source/com/javabeginnersguide/chapter03/Guess2.java
@@ -0,0 +1,16 @@
+package com.javabeginnersguide.chapter03;
+
+// Игра в угадывание буквы, версия 2.
+class Guess2 {
+    public static void main(String[] args)
+            throws java.io.IOException {
+
+        char ch, answer = 'K';
+        System.out.println("Задумана буква между А и Z.");
+        System.out.print("Попробуйте ее угадать: ");
+        ch = (char) System.in.read();   // чтение символа с клавиатуры
+
+        if(ch == answer) System.out.println("** Правильно **");
+        else System.out.println("...Увы, не угадали.");
+    }
+}
