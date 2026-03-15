f4b61ebe58bb9a7b7f98b6f7c26f52a676da07dd
maksimmerkulov <merkulov.mv@outlook.com>
2026-03-15 11:45:00 +0500

    Глава 3: Некоторые разновидности цикла `for` (Операторы управления программой)

---
+++ source/com/javabeginnersguide/chapter03/ForTest.java
@@ -0,0 +1,13 @@
+package com.javabeginnersguide.chapter03;
+
+// Цикл до тех пор, пока не будет введена буква S.
+class ForTest {
+    public static void main(String[] args)
+            throws java.io.IOException {
+        int i;
+        System.out.println("Для остановки цикла нажмите S.");
+
+        for(i = 0; (char) System.in.read() != 'S'; i++)
+            System.out.println("Проход #" + i);
+    }
+}
