5131cb935d492619563de017c059985377d08013
maksimmerkulov <merkulov.mv@outlook.com>
2026-03-15 14:15:00 +0500

    Глава 3: Циклы без тела (Операторы управления программой)

---
+++ source/com/javabeginnersguide/chapter03/Empty3.java
@@ -0,0 +1,14 @@
+package com.javabeginnersguide.chapter03;
+
+// Тело цикла может быть пустым.
+class Empty3 {
+    public static void main(String[] args) {
+        int i;
+        int sum = 0;
+
+        // Просуммировать числа от 1 до 5.
+        for(i = 1; i <= 5; sum += i++);
+
+        System.out.println("Сумма равна " + sum);
+    }
+}
