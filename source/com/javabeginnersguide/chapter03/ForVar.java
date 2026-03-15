2bb67e49c1ca1b4b96a89eb9e4a7d0945aa8e2b4
maksimmerkulov <merkulov.mv@outlook.com>
2026-03-15 14:30:00 +0500

    Глава 3: Объявление переменных управления циклом внутри цикла `for` (Операторы управления программой)

---
+++ source/com/javabeginnersguide/chapter03/ForVar.java
@@ -0,0 +1,19 @@
+package com.javabeginnersguide.chapter03;
+
+// Объявление переменной управления циклом внутри оператора for.
+class ForVar {
+    public static void main(String[] args) {
+        int sum = 0;
+        int fact = 1;
+
+        // Вычислить факториал чисел от 1 до 5.
+        for(int i = 1; i <= 5; i++) {
+            sum += i;  // переменная i известна во всем цикле
+            fact *= i;
+        }
+
+        // Но уже здесь переменная i не известна.
+        System.out.println("Сумма равна " + sum);
+        System.out.println("Факториал равен " + fact);
+    }
+}
